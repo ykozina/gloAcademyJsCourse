a2f282f66dfe76f6f6755b8d08e049fdce111969
Julia Kozina <ykozina@yandex.ru>
2025-03-14 02:49:16 +0300

    в скрипт добавлены переменные, алерт и вывод в консоль

---
+++ script.js
@@ -0,0 +1,10 @@
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+alert('На данной странице выводится задание первого урока курса');
+
+console.log('Первое задание выполнено!')
